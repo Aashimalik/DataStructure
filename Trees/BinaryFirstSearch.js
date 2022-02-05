97c06b2668581e6e48ab39f736f248871c04312e
Ayesha Malik <ayeshamalik8751@gmail.com>
2022-02-05 10:00:55 +0000

    Trees

---
+++ Trees/BinaryFirstSearch.js
@@ -0,0 +1,79 @@
+
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+//     9
+//  4     20
+//1  6  15  170
+class BinarySearchTree {
+  constructor() {
+     this.root = null;
+  }
+
+  insert(value){
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while(true){
+        if(value < currentNode.value){
+          //Left
+          if(!currentNode.left){
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          //Right
+          if(!currentNode.right){
+            currentNode.right = newNode;
+            return this;
+          } 
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  BFS(){
+    let node = this.root,
+        data = [],
+        queue = [];
+    queue.push(node);
+
+    while(queue.length) {
+        node = queue.shift();
+        data.push(node.value);
+        if(node.left) queue.push(node.left);
+        if(node.right) queue.push(node.right);
+    }
+      return data;
+    }
+
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(tree.BFS());
+
+console.log(JSON.stringify(traverse(tree.root)));
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
+
